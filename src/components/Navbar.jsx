c3fe3e85aa10878e21aecfc7624e4fe1ebb32663
Kirtan134 <kirtan134parikh@gmail.com>
2024-06-06 23:39:08 +0530

    added some improvements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,17 +12,16 @@ const Navbar = () => {
           <div className="flex gap-5">
             <img className='w-10' src="./icons/logo.png" alt="logo" />
             <div className="logo font-extralight text-2xl">
-        <span className="text-black font-arvo-regular-italic font-serif">&lt;Pass</span>
-            Fortress
-            <span className="text-black font-serif">/ &gt;</span>
-            
+              <span className="text-black font-arvo-regular-italic font-serif">&lt;Pass</span>
+              Fortress
+              <span className="text-black font-serif">/ &gt;</span>
             </div>
           </div>
 
-        <button className="text-white bg-black my-5 rounded-full flex justify-between items-center ring-white ring-1">
-            <img className='invert p-1 w-10 rounded-full' src="./icons/github.png" alt="github logo" />
-            <span className="font-bold px-2">Github</span>
-        </button>
+        <a href="https://github.com/Kirtan134/PassFortress" target="_blank" rel="noopener noreferrer" className="text-white bg-black my-5 rounded-full flex justify-between items-center ring-white ring-1">
+          <img className='invert p-1 w-10 rounded-full' src="./icons/github.png" alt="github logo" />
+          <span className="font-bold px-2">Github</span>
+        </a>
       </div>
     </nav>
   );
