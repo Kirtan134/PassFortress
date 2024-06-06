a94aa496c3e4633674700c19203d47a5cd2415f6
Kirtan134 <kirtan134parikh@gmail.com>
2024-06-06 23:28:07 +0530

    deployment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="bg-slate-50 text-black flex flex-col justify-center items-center fixed bottom-0 w-full">
       <div className="flex gap-2">
-        <img className="w-10" src="/icons/logo.png" alt="logo" />
+        <img className="w-10" src="./icons/logo.png" alt="logo" />
         <div className="logo font-extralight text-2xl">
           <span className="text-black font-arvo-regular-italic font-serif">
             &lt;Pass
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
       <div className="mycontainer flex justify-between items-center px-4 py-5 h-14  -z-10 ">
         
           <div className="flex gap-5">
-            <img className='w-10' src="/icons/logo.png" alt="logo" />
+            <img className='w-10' src="./icons/logo.png" alt="logo" />
             <div className="logo font-extralight text-2xl">
         <span className="text-black font-arvo-regular-italic font-serif">&lt;Pass</span>
             Fortress
@@ -20,7 +20,7 @@ const Navbar = () => {
           </div>
 
         <button className="text-white bg-black my-5 rounded-full flex justify-between items-center ring-white ring-1">
-            <img className='invert p-1 w-10 rounded-full' src="/icons/github.png" alt="github logo" />
+            <img className='invert p-1 w-10 rounded-full' src="./icons/github.png" alt="github logo" />
             <span className="font-bold px-2">Github</span>
         </button>
       </div>
